6292776303d701d1ea941e94b55377e49cb0c4f0
Numaan <numaan911098@gmail.com>
2024-01-21 10:45:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+Jobs = [
+    {
+        'title': 'Software Engineer',
+        'company': 'Google',
+        'salary': '100'   
+    },
+
+    {
+        'title': 'Data Scientist',
+        'company': 'Facebook',
+        'salary': '200'
+    }
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', jobs=Jobs)
+
+@app.route('/api/jobs')
+def about():
+    return jsonify(Jobs)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port= 8081,debug=True)
